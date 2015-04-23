6e46e0025e49d5f123adcf82483c6654b85276be
Johanny LS <johanny.dls@gmail.com>
2015-04-23 03:09:39 -0300

    Dinamica finalizada. Pequenas mudancas nas listas

--- dinamica/js/listadinamica.js
+++ dinamica/js/listadinamica.js
@@ -2,7 +2,7 @@ function addItem() {
     if (document.getElementById('tema-aprender').value != "") {
         var value = $('#tema-aprender').val();
 
-        $("#linhas1").append('<tr> <td width=200>' + value + '</td> <td width=125><button class="btn btn-small btn-danger" onclick="RemoveTableRow(this)" type="button">Remover</button></td> <td width=125><button class="btn btn-small btn-success" onclick="MoveTableRow(this)" type="button">Aprendi!</button></td>  </tr>');
+        $("#linhas1").append('<tr> <td width=200>' + value + '</td> <td width=125><button class="btn btn-small btn-danger" onclick="RemoveTableRow(this)" type="button">Remover</button></td> <td width=125><button class="btn btn-small btn-success" onclick="MoveTableRow(this)" type="button">Aprendi</button></td>  </tr>');
     }
     document.getElementById('tema-aprender').value = "";
 }
